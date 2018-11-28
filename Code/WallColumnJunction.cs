c2014c0fa440aa4f8708bd24600e5ca40a7a98bb
Xuebing <xbid@qq.com>
2018-11-28 19:42:12 +0800

    add wall column junction.

---
+++ Code/WallColumnJunction.cs
@@ -0,0 +1,69 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+
+namespace ClassLibrary1
+{
+  [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+  public class Class: IExternalCommand
+  {
+    public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+    {
+
+      UIDocument uidoc = commandData.Application.ActiveUIDocument;
+      Document document = uidoc.Document;
+      FilteredElementCollector columnCollecter = new FilteredElementCollector(document);
+      columnCollecter.OfCategory(BuiltInCategory.OST_StructuralColumns).OfClass(typeof(FamilyInstance));
+      List<FamilyInstance> columnlist = columnCollecter.ToList().ConvertAll(x => x as FamilyInstance);
+      foreach (FamilyInstance column in columnlist)
+      {
+        ElementIntersectsElementFilter columnIntersectsFilter = new ElementIntersectsElementFilter(column);
+        FilteredElementCollector filteredElementCollector = new FilteredElementCollector(document);
+        List<Element> columnsIntersectList = filteredElementCollector.WherePasses(columnIntersectsFilter).ToList();
+
+        var coulmnIntersectList = from elem in columnsIntersectList
+                                  where elem.Category.Id == new ElementId(-2001320)//梁
+                                  ||
+                                  elem.Category.Id == new ElementId(-2001330)//柱
+                                  ||
+                                   elem.Category.Id == new ElementId(-2000011) && (elem as Wall).WallType.Kind == WallKind.Basic//基本墙
+                                  ||
+                                  elem.Category.Id == new ElementId(-2000032)//板
+                                  select elem;
+        foreach (Element elem in columnsIntersectList)
+        {
+          Transaction ts = new Transaction(document);
+          ts.Start("cut");
+          if (JoinGeometryUtils.AreElementsJoined(document, column, elem) == false)
+          {
+            try
+            {
+              JoinGeometryUtils.JoinGeometry(document, elem, column);
+            }
+            catch
+            {
+
+            }
+
+
+          }
+          else
+          {
+            if (JoinGeometryUtils.IsCuttingElementInJoin(document, column, elem) == false)
+            {
+              JoinGeometryUtils.SwitchJoinOrder(document, column, elem);
+            }
+
+          }
+          ts.Commit();
+        }
+      }
+      return Result.Succeeded;
+    }
+  }
+}
---
+++ Code/Wallconnect.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.DB.Structure;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WorkCode
+{
+  [Transaction(TransactionMode.Manual)]
+  public class Wallconnect : IExternalCommand
+  {
+    public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+    {
+
+      UIApplication uiApp = commandData.Application;
+      UIDocument uiDoc = uiApp.ActiveUIDocument;
+      Document doc = uiApp.ActiveUIDocument.Document;
+      Application app = uiApp.Application;
+
+      Selection selection = uiDoc.Selection;
+
+      Transaction ts = new Transaction(doc, "cut");
+      ts.Start();
+
+      IList<Element> _elements = uiDoc.Selection.PickElementsByRectangle(new WallFilter(), "请框选所有需要剪切的墙、板、柱");
+
+      foreach (var element1 in _elements)
+      {
+      
+        var collector2 = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_StructuralFraming).OfClass(typeof(FamilyInstance));
+        var pipeIntersectFilter = new ElementIntersectsElementFilter(element1);
+
+        List<FamilyInstance> pipes = collector2.WherePasses(pipeIntersectFilter).ToList().ConvertAll(x => x as FamilyInstance);
+
+        foreach (var pipe in pipes)
+        {
+          InstanceVoidCutUtils.AddInstanceVoidCut(doc, element1, pipe);
+        }
+        new BoundingBoxIsInsideFilter();
+        
+      }
+
+      ts.Commit();
+      return Result.Succeeded;
+    }
+  }
+
+}
